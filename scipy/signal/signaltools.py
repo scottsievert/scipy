d809bd4f933f3526b8c0b34b50c413918714c55b
Scott Sievert <me@scottsievert.com>
2016-02-26 08:21:40 -0600

    DOC: Adds comment about catching integer precision
    + covers floating point precision more cleanly
    
    + adds comment about catching integer precision

--- scipy/signal/signaltools.py
+++ scipy/signal/signaltools.py
@@ -209,7 +209,9 @@ def correlate(in1, in2, mode='full', method='auto'):
         else:
             method = 'direct'  # for non-numeric arrays
     elif ((in1.dtype.kind in 'ui' or in2.dtype.kind in 'ui') and 
-          np.abs(in1).sum() * np.abs(in2).sum() > 2**52 - 1):
+          max(in1.max(), in2.max()) * max(in1.size, in2.size) > 2**52 - 1):
+        # catch when more precision required than float provides (representing
+        # a integer as float can lose precision if integer larger than 2**52)
         method = 'direct'
 
     # this either calls fftconvolve or this function with method=='direct'
